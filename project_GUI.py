edf009cc9a9dc88e7f610a7ca2db3cf4aba35b8e
SeanEmery4 <41141351+SeanEmery4@users.noreply.github.com>
2020-04-27 16:06:52 +1000

    Update project_GUI.py

--- project_GUI.py
+++ project_GUI.py
@@ -21,22 +21,50 @@ canvas.pack()
 
 ## Variables
 global mode
+global F_C_Dist_GUI
+global DetectButtonGUI
+global AutoButtonGUI
+global OffButtonGUI
+global ModeDescGUI
+global F_C_Object_GUI
 
 ## Functions
 
 def getDistance():
     responce = requests.get('https://api.particle.io/v1/devices/e00fce6825acdbf3eaef2482/F_C_Dist?access_token=05a78f428e275ea748d57326857802757ed4ead0')
     respJSON = responce.json()
-    dist = respJSON.get("result")
-    return dist
+    try:
+        dist = int(respJSON.get("result"))
+        if dist > 80:
+            dist = 81
+        elif dist < 10:
+            dist = 10
+        return dist
+    except:
+        return "Error"
 
 def system():
-    F_C_Dist = getDistance()
-    canvas.create_text(100, 350, font = SubheadingFont, text = F_C_Dist)
+    global F_C_Dist_GUI, F_C_Object_GUI
+    while True:
+        F_C_Dist = getDistance()
+        canvas.delete(F_C_Dist_GUI, F_C_Object_GUI)
+        if F_C_Dist == "Error":
+            F_C_Dist_GUI = canvas.create_text(100, 350, font = SubheadingFont, fill = 'red', text = F_C_Dist)
+        elif F_C_Dist < 80 and F_C_Dist >= 50:
+            F_C_Object_GUI = canvas.create_rectangle(405, 40, 445, 105, fill = 'orange', outline = 'orange')
+            F_C_Dist_GUI = canvas.create_text(100, 350, font = SubheadingFont, fill = 'red', text = F_C_Dist)
+        elif F_C_Dist < 50:
+            F_C_Object_GUI = canvas.create_rectangle(405, 40, 445, 105, fill = 'red', outline = 'red')
+            F_C_Dist_GUI = canvas.create_text(100, 350, font = SubheadingFont, fill = 'red', text = F_C_Dist)
+        else:
+            F_C_Object_GUI = canvas.create_rectangle(405, 40, 445, 105, fill = 'green', outline = 'green')
+            F_C_Dist_GUI = canvas.create_text(100, 350, font = SubheadingFont, fill = 'green', text = "Clear")
+
     
 def setCanvas():
+    global F_C_Dist_GUI
     # clear cnavas to redraw
-    canvas.delete("all")
+    F_C_Dist = getDistance()
     
     #create sections
     canvas.create_rectangle(3, 3, 712, 37, fill = 'grey65', outline = 'grey60')
@@ -48,7 +76,12 @@ def setCanvas():
     # create data section
     canvas.create_text(70, 315, font = SubheadingFont, text = "Obstacle Data")
     canvas.create_text(40, 350, font = SubheadingFont, text = "Front: ")
-    system()
+    
+    if F_C_Dist < 80:
+        F_C_Dist_GUI = canvas.create_text(100, 350, font = SubheadingFont, fill = 'red', text = F_C_Dist)
+    else:
+        F_C_Dist_GUI = canvas.create_text(100, 350, font = SubheadingFont, fill = 'green', text = "Clear")
+    
     canvas.create_text(41, 375, font = SubheadingFont, text = "Rear: ")
     #create car
     canvas.create_rectangle(350, 110, 500, 360, fill = 'blue2', outline = 'blue') #body
@@ -61,28 +94,47 @@ def setCanvas():
 
 
 def setModeOff():
+    global DetectButtonGUI
+    global AutoButtonGUI
+    global OffButtonGUI
+    global ModeDescGUI
+    
     mode = 0
-    setCanvas()
-    canvas.create_window(90, 97, window = DetectModeOffButton)
-    canvas.create_window(90, 147, window = AutoModeOffButton)
-    canvas.create_window(90, 197, window = OffModeButton)
-    canvas.create_text(80, 57, font = SubheadingFont, text = "Off")
+    # clear canvas to redraw
+    canvas.delete(DetectButtonGUI, AutoButtonGUI, OffButtonGUI, ModeDescGUI)
+    DetectButtonGUI = canvas.create_window(90, 97, window = DetectModeOffButton)
+    AutoButtonGUI = canvas.create_window(90, 147, window = AutoModeOffButton)
+    OffButtonGUI = canvas.create_window(90, 197, window = OffModeButton)
+    ModeDescGUI = canvas.create_text(80, 57, font = SubheadingFont, text = "Off")
   
 def setModeDetect():
+    global DetectButtonGUI
+    global AutoButtonGUI
+    global OffButtonGUI
+    global ModeDescGUI
+    
     mode = 1
-    setCanvas()
-    canvas.create_window(90, 97, window = DetectModeOnButton)
-    canvas.create_window(90, 147, window = AutoModeOffButton)
-    canvas.create_window(90, 197, window = OffModeButton)
-    canvas.create_text(105, 57, font = SubheadingFont, text = "Detection")
+    
+    canvas.delete(DetectButtonGUI, AutoButtonGUI, OffButtonGUI, ModeDescGUI)
+    DetectButtonGUI = canvas.create_window(90, 97, window = DetectModeOnButton)
+    AutoButtonGUI = canvas.create_window(90, 147, window = AutoModeOffButton)
+    OffButtonGUI = canvas.create_window(90, 197, window = OffModeButton)
+    ModeDescGUI = canvas.create_text(105, 57, font = SubheadingFont, text = "Detection")
     
 def setModeAuto():
-    mode = 1
-    setCanvas()
-    canvas.create_window(90, 97, window = DetectModeOffButton)
-    canvas.create_window(90, 147, window = AutoModeOnButton)
-    canvas.create_window(90, 197, window = OffModeButton)
-    canvas.create_text(120, 57, font = SubheadingFont, text = "Autonomous")
+    global DetectButtonGUI
+    global AutoButtonGUI
+    global OffButtonGUI
+    global ModeDescGUI
+    
+    mode = 2
+    
+    canvas.delete(DetectButtonGUI, AutoButtonGUI, OffButtonGUI, ModeDescGUI)
+
+    DetectButtonGUI = canvas.create_window(90, 97, window = DetectModeOffButton)
+    AutoButtonGUI = canvas.create_window(90, 147, window = AutoModeOnButton)
+    OffButtonGUI = canvas.create_window(90, 197, window = OffModeButton)
+    ModeDescGUI = canvas.create_text(120, 57, font = SubheadingFont, text = "Autonomous")
    
 def close():
     GPIO.cleanup()
@@ -98,8 +150,16 @@ AutoModeOffButton = Button(win, text = "Autonomous Mode", command = setModeAuto,
 AutoModeOnButton = Button(win, text = "Autonomous Mode", command = setModeAuto, bg = 'green', height = 2, width = 15)
 OffModeButton = Button(win, text = "System Off", command = setModeOff, bg = 'red', height = 2, width = 15)
 
-setModeOff()
+DetectButtonGUI = canvas.create_window(90, 97, window = DetectModeOffButton)
+AutoButtonGUI = canvas.create_window(90, 147, window = AutoModeOffButton)
+OffButtonGUI = canvas.create_window(90, 197, window = OffModeButton)
+ModeDescGUI = canvas.create_text(80, 57, font = SubheadingFont, text = "Off")
+F_C_Object_GUI = canvas.create_rectangle(405, 40, 445, 105)
+
+systemThread = Thread(target = system)
+systemThread.start()
 
 win.protocol("WM_DELETE_WINDOW", close)
 
 win.mainloop()
+
